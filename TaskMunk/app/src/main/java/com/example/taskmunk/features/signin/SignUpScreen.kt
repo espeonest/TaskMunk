1115469f3b669203bdd884897ef844e2c89918d1
espeonest <145805393+espeonest@users.noreply.github.com>
2025-11-27 22:59:21 -0500

    Uploaded base project

---
+++ TaskMunk/app/src/main/java/com/example/taskmunk/features/signin/SignUpScreen.kt
@@ -0,0 +1,17 @@
+package com.example.taskmunk.features.signin
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SignUpScreen(modifier: Modifier = Modifier){
+    Text("Testing")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSignUp(){
+    SignUpScreen()
+}
