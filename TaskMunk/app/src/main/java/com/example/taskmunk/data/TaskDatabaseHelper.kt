2fe4dce99d9021a1e753356e8d8392f235899cbe
BrittanyRogers <bmrogers@conestogac.on.ca>
2025-12-01 19:30:31 -0500

    Create CRUD and create in Database Helper for local SQLite

--- TaskMunk/app/src/main/java/com/example/taskmunk/data/TaskDatabaseHelper.kt
+++ TaskMunk/app/src/main/java/com/example/taskmunk/data/TaskDatabaseHelper.kt
@@ -1,5 +1,7 @@
 package com.example.taskmunk.data
 
+import android.app.ActivityManager
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -10,7 +12,7 @@ class TaskDatabaseHelper(context: Context):
     override fun onCreate(db: SQLiteDatabase?){
         db?.execSQL("""
             CREATE TABLE tasks(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id TEXT PRIMARY KEY AUTOINCREMENT,
             title TEXT,
             description TEXT,
             dueDate TEXT,
@@ -32,4 +34,76 @@ class TaskDatabaseHelper(context: Context):
         db?.execSQL("DROP TABLE IF EXISTS notes")
         onCreate(db)
     }
+
+    //Create - Insert a new Task
+    fun insertTask(title: String, description: String, dueDate: String, priority: String, category: String, status: String, reminder: String, dateCreated: String, dateCompleted: String){
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put("title", title)
+            put("description", description)
+            put("dueDate", dueDate)
+            put("priority", priority)
+            put("category", category)
+            put("status", status)
+            put("reminder", reminder)
+            put("dateCreated", dateCreated)
+            put("dateCompleted", dateCompleted)
+        }
+        db.insert("tasks", null, values)
+        db.close()
+    }
+
+    //Read - Retrieve all tasks
+    fun getAllTasks(): List<Task> {
+        val tasks = mutableListOf<Task>()
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM tasks", null)
+        while (cursor.moveToNext()) {
+            tasks.add(
+                Task(
+                    cursor.getString(0),
+                    cursor.getString(1),
+                    cursor.getString(2),
+                    cursor.getString(3),
+                    cursor.getString(4),
+                    cursor.getString(5),
+                    cursor.getString(6),
+                    cursor.getString(7),
+                    cursor.getString(8),
+                    cursor.getString(9)
+                )
+            )
+        }
+        cursor.close()
+        db.close()
+
+        return tasks
+    }
+
+    //Update - Modify selected Task
+    fun updateTask(id: String, title: String, description: String, dueDate: String, priority: String, category: String, status: String, reminder: String, dateCreated: String, dateCompleted: String){
+        val db = writableDatabase
+        //New values using ContentValues object to store
+        val values = ContentValues().apply{
+            put("title", title)
+            put("description", description)
+            put("dueDate", dueDate)
+            put("priority", priority)
+            put("category", category)
+            put("status", status)
+            put("reminder", reminder)
+            put("dateCreated", dateCreated)
+            put("dateCompleted", dateCompleted)
+        }
+        db.update("tasks", values,"id=?", arrayOf(id.toString()))
+        db.close()
+    }
+
+    //Delete - Remove selected Task
+    fun deleteTask(id: String){
+        val db = writableDatabase
+        db.delete("tasks", "id=?", arrayOf(id.toString()))
+        db.close()
+    }
 }
+
