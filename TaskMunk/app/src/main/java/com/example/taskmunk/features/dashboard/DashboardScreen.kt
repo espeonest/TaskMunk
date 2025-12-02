f79252c9bb50ef5e12ad70c4fa3bff394c67586b
BrittanyRogers <bmrogers@conestogac.on.ca>
2025-12-01 20:05:20 -0500

    Working on Dashboard Screen, started top bar

--- TaskMunk/app/src/main/java/com/example/taskmunk/features/dashboard/DashboardScreen.kt
+++ TaskMunk/app/src/main/java/com/example/taskmunk/features/dashboard/DashboardScreen.kt
@@ -1,8 +1,71 @@
 package com.example.taskmunk.features.dashboard
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.pm.ShortcutInfoCompat
+import com.example.taskmunk.R
+import com.example.taskmunk.features.signin.SignUpScreen
 
 @Composable
-fun DashboardScreen(){
+fun DashboardScreen(modifier: Modifier){
 
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ){
+        Column {
+            //Top bar
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(175.dp),
+                color = MaterialTheme.colorScheme.primary,
+                shadowElevation = 5.dp
+            ) {
+                Row(
+                    modifier = Modifier
+                      .padding(horizontal = 16.dp)
+                        .fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+
+
+                ){
+                    Image(
+                    painter = painterResource(R.drawable.logo),
+                    contentDescription = stringResource(R.string.app_name),
+                    modifier = Modifier.size(60.dp)
+                )
+                    Spacer(modifier = Modifier.width(15.dp))
+
+                    Text(stringResource(R.string.app_name),
+                        style = MaterialTheme.typography.displayLarge,
+                        color = MaterialTheme.colorScheme.onPrimary)
+                }
+
+
+
+            }
+        }
 }
+}
+
