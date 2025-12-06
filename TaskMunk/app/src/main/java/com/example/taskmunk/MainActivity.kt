14740274dae51826567b6befe82d1221bd9e8bb7
faolan <faolanschaefer@gmail.com>
2025-12-06 16:23:00 -0500

    Created global top bar

--- TaskMunk/app/src/main/java/com/example/taskmunk/MainActivity.kt
+++ TaskMunk/app/src/main/java/com/example/taskmunk/MainActivity.kt
@@ -7,18 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.taskmunk.features.dashboard.DashboardViewModel
-import com.example.taskmunk.features.home.HomeScreen
-import com.example.taskmunk.features.signin.SignUpScreen
 import com.example.taskmunk.features.tasks.TaskViewModel
 import com.example.taskmunk.navigation.AppNavigation
+import com.example.taskmunk.navigation.GlobalTopBar
 import com.example.taskmunk.ui.theme.TaskMunkTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +26,10 @@ class MainActivity : ComponentActivity() {
 
                 val dashboardViewModel: DashboardViewModel = viewModel ()
                 val taskViewModel: TaskViewModel = viewModel ()
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = { GlobalTopBar(navController = navController) },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     AppNavigation(navController = navController, dashboardViewModel = dashboardViewModel, taskViewModel = taskViewModel, modifier = Modifier.padding(innerPadding))
                 }
             }
